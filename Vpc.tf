d1ed455a30625214da86bc0c0d9d9e700d7e43c7
BryanL <lagunabs2601@gmail.com>
2026-03-01 17:26:54 -0500

    create Nat and Igw

--- Vpc.tf
+++ Vpc.tf
@@ -5,3 +5,31 @@ resource "aws_vpc" "myvpc" {
         Name = "Vpc-tf1"
     }
 }
+
+resource "aws_internet_gateway" "myitg" {
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+      Name = "onlyigw"
+    }
+  
+}
+
+resource "aws_nat_gateway" "mynat" {
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+      Name = "natgate1"
+    }
+  
+}
+
+resource "aws_nat_gateway_eip_association" "eipassoc" {
+  nat_gateway_id = aws_nat_gateway.mynat.id
+  allocation_id = aws_eip.eipfornat.id
+
+  depends_on = [ aws_internet_gateway.myitg ]
+}
+
+resource "aws_eip" "eipfornat" {
+    domain = "vpc"
+  
+}
---
+++ routetables.tf
@@ -0,0 +1,3 @@
+resource "aws_routet" "name" {
+  
+}
